0c9bddf79cb8a9fffd0e45ab8d42bc88486f4027
Jason McPeak <jmcpeak@payrailz.com>
2020-10-12 10:05:12 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Button = React.lazy(() => import('@material-ui/core/Button'));
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>
+        <Suspense fallback={<span style={{ height: 36, width: 130 }} />}>
+          <Button variant="contained">Hello World</Button>
+        </Suspense>
       </header>
     </div>
   );
-}
+};
 
 export default App;
